fd154c698645d3e5da84f1b48e3923e7ccda0eb5
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-04 14:59:17 +0700

    commit3

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama file : ";
+    cin >>  NamaFile;
+
+    ofstream outfile;
+
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    
+    while (true) {
+        cout << "- ";
+
+        getline(cin, baris);
+
+        if (baris == "q") break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+
+    ifstream infile;
+
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+
+        while (getline(infile, baris))
+        {
+
+            cout << baris << '\n';
+        }
+
+        infile.close();    
+    }
+    else cout << "Unable to open file";
+    return 0;
+};
