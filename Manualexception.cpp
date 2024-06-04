1e4ef57d4d3ec5b77c75e024e59e4a6b6d8abbde
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-04 15:20:20 +0700

    commit8

--- Manualexception.cpp
+++ Manualexception.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah interger maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        
+    }
+}
