a60306b060fbb9653bf20015a3f4c0f6062326ec
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-06-04 15:10:59 +0700

    commit6

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -19,4 +19,7 @@ int main()
         /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
     }
 
+    cout << "Baris Program Yang terakhir" << endl;
+    /*penanda 2 : bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 };
